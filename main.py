5c76c3ae2b8418f99739c02d1451570f6eca0b2c
Nihal Singh <nihal.111@gmail.com>
2015-12-22 11:44:21 +0530

    Robusty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import eyed3
+from eyed3 import id3
 import subprocess
 import subprocess
 import mechanize
@@ -13,66 +14,74 @@ apikey="v6m4z1dh"
 
 
 
-filename="c.mp3"
+filename="a.mp3"
 
+flag=1
+try:
+	mp3 = eyed3.load(filename)
+except:
+	flag=0
+if (flag==1):
+	p = subprocess.Popen(['fpcalc',filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err = p.communicate()
+	#print out
 
+	a = out.find("DURATION", 0,40) +9
+	b = out.find("FINGERPRINT", 0 ,40)-1
 
-mp3 = eyed3.load(filename)
-p = subprocess.Popen(['fpcalc',filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = p.communicate()
-#print out
+	duration = out[a:b]
+	fingerprint = out[b+13:-1]
+	url = 'http://api.acoustid.org/v2/lookup?client='+apikey+'&meta=recordings+releasegroups+compress&duration='+duration+'&fingerprint='+fingerprint
+	#print url
+	info= br.open(url).read()
+	if(info.find("error")==-1):
+		artistindex0=info.find("artist")
+		artistindex1= info.find("name")+8
+		artistindex2= info.find("\"",artistindex1)
+		albumindex1= info.find("Album")
+		albumindex1= info.find("title",albumindex1)+9
+		albumindex2= info.find("\"",albumindex1)
+		titleindex1 = albumindex2 + 15
+		titleindex2 = info.find("\"",titleindex1)
+		artist = info[(artistindex1):artistindex2]
+		album = info[albumindex1:albumindex2]
+		title =info[titleindex1:titleindex2]
 
-a = out.find("DURATION", 0,40) +9
-b = out.find("FINGERPRINT", 0 ,40)-1
+		while ((artist=="Various Artists") and ((artistindex1+400)<len(info))):
+			artistindex0=info.find("artist",artistindex2)
+			artistindex1= info.find("name",artistindex0)+8
+			artistindex2= info.find("\"",artistindex1)
+			artist = info[(artistindex1):artistindex2]
 
-duration = out[a:b]
-fingerprint = out[b+13:-1]
+		url = 'http://search.azlyrics.com/search.php?q='+artist+'+'+title
+		url =url.replace(' ','+')
 
-url = 'http://api.acoustid.org/v2/lookup?client='+apikey+'&meta=recordings+releasegroups+compress&duration='+duration+'&fingerprint='+fingerprint
-info= br.open(url).read()
-artistindex1= info.find("name")
-artistindex2= info.find("\"",artistindex1+8)
-albumindex1= info.find("Album")
-albumindex1= info.find("title",albumindex1)+9
-albumindex2= info.find("\"",albumindex1)
-titleindex1 = albumindex2 + 15
-titleindex2 = info.find("\"",titleindex1)
-artist = info[(artistindex1+8):artistindex2]
-album = info[albumindex1:albumindex2]
-title =info[titleindex1:titleindex2]
+		br.open(url)
 
-url = 'http://search.azlyrics.com/search.php?q='+artist+'+'+title
-url =url.replace(' ','+')
+		links = []
+		for link in br.links(url_regex="http://www.azlyrics.com/lyrics/"):
+			links.append(link.url)
+		if (len(links)):
+			#print links[0]
+			info= br.open(links[0]).read()
+			albumindex1 = info.find("glyphicon-cd")
+			if (albumindex1!=-1):		
+				albumindex1 = info.find("collapse",albumindex1)+11
+				albumindex2 = info.find("\"",albumindex1)
+				yearindex1 = info.find("(",albumindex2)+1
+				yearindex2 = info.find(")",albumindex2)
+				album = info[albumindex1:albumindex2]
+				year = (info[yearindex1:yearindex2])
+				print album
+				print year
+				mp3.tag.album = unicode(album, "UTF-8")
+				try:
+					mp3.tag.recording_date=year
+				except:
+					print "date haga"
 
-br.open(url)
-
-links = []
-for link in br.links(url_regex="http://www.azlyrics.com/lyrics/"):
-	links.append(link.url)
-if (len(links)):
-	info= br.open(links[0]).read()
-	albumindex1 = info.find("glyphicon-cd")
-	albumindex1 = info.find("collapse",albumindex1)+11
-	albumindex2 = info.find("\"",albumindex1)
-	yearindex1 = info.find("(",albumindex2)+1
-	yearindex2 = info.find(")",albumindex2)
-	album = info[albumindex1:albumindex2]
-	year = int(info[yearindex1:yearindex2])
-	print album
-	print year
-	mp3.tag.album = unicode(album, "UTF-8")
-	try:
-		mp3.tag.recording_date=year
-	except:
-		print "date haga"
-
-
-print artist
-print title
-
-
-mp3.tag.artist = unicode(artist, "UTF-8")
-
-mp3.tag.title = unicode(title, "UTF-8")
-
-mp3.tag.save()
+		print artist
+		print title
+		mp3.tag.artist = unicode(artist, "UTF-8")
+		mp3.tag.title = unicode(title, "UTF-8")
+		mp3.tag.save()
